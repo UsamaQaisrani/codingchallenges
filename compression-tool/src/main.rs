c8832b3189eca011dc6053ef218a7d6c91a8f98d
Muhammad <usamaqaisranifps@gmail.com>
2026-08-29 21:30:11 +0100

    added: write compressed output with header and bit-packed encoding

--- compression-tool/src/main.rs
+++ compression-tool/src/main.rs
@@ -3,11 +3,21 @@ use compression_tool::encoder::Encoder;
 
 #[derive(Parser)]
 struct Args {
-    file_path: Option<String>,
+    #[arg(short = 'o')]
+    output_file_path: String,
+
+    input_file_path: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
-    let encoder = Encoder {};
-    let _res = encoder.encode(args.file_path.as_deref());
+    let encoder = Encoder::new(args.input_file_path.as_deref(), args.output_file_path);
+
+    match encoder.encode() {
+        Ok(()) => println!("Encoded successfully"),
+        Err(err) => {
+            println!("{}", err);
+            std::process::exit(1)
+        }
+    }
 }
