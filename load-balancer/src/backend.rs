b76cf63306439f35ec93c22a4c926013e225c507
Muhammad <usamaqaisranifps@gmail.com>
2026-09-09 12:42:27 +0100

    modified: take ports from cli

--- load-balancer/src/backend.rs
+++ load-balancer/src/backend.rs
@@ -6,14 +6,13 @@ struct AppState {
     id: i32,
 }
 
-pub async fn start(id: i32) -> Result<(), anyhow::Error> {
+pub async fn start(id: i32, port: u32) -> Result<(), anyhow::Error> {
     let shared_state = Arc::new(AppState { id });
     let app = Router::new()
         .route("/", get(hello))
         .with_state(shared_state);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await?;
-    let addr = listener.local_addr()?;
-    println!("Backend Server #{} Listening on {}", id, addr);
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+    println!("Backend Server #{} Listening on {}", id, port);
     axum::serve(listener, app).await?;
     Ok(())
 }
--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -1,9 +1,20 @@
+use clap::Parser;
 use load_balancer::backend::start;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short = '1')]
+    port1: u32,
+
+    #[arg(short = '2')]
+    port2: u32,
+}
 
 #[tokio::main]
 async fn main() {
-    let be1 = tokio::spawn(start(1));
-    let be2 = tokio::spawn(start(2));
+    let args = Args::parse();
+    let be1 = tokio::spawn(start(1, args.port1));
+    let be2 = tokio::spawn(start(2, args.port2));
 
     let (be1, be2) = tokio::join!(be1, be2);
 
